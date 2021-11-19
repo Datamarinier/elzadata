a024a1238e50f750c8d27b53de25d0b6ba461eb9
FrederikHeylenDM <frederik.heylen@datamarinier.be>
2021-11-19 10:26:19 +0100

    Fix postcodes, total population

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -5,24 +5,24 @@ library(tibble)
 #
 postcodes <- tribble(
                 ~gemeente,   ~postcode,    ~elz,          ~stad, ~bev_2021, ~benoeming,
-                  "Schoten",       2900,    "ELZ Noord",      0, 34316,   "Schoten (2900)",
-                  "Stabroek",      2940,    "ELZ Noord",      0, 18789,    "Stabroek (2940)",
-                  "Wommelgem",     2160,    "ELZ Oost",       0, 12943,    "Wommelgem (2160)",
+                  "Schoten",       2900,    "ELZ Noord",      0, 34319,    "Schoten (2900)",
+                  "Stabroek",      2940,    "ELZ Noord",      0, 18692,    "Stabroek (2940)",
+                  "Wommelgem",     2160,    "ELZ Oost",       0, 12994,    "Wommelgem (2160)",
                   "Borsbeek",      2150,    "ELZ ZORA",       0, 11077,    "Borsbeek (2150)",
-                  "Antwerpen",     2000,    "ELZ Centrum",    1, 39382,    "Antwerpen Centrum (2000)",
-                  "Antwerpen",     2018,    "ELZ Centrum",    1, 59258,    "Antwerpen Zuid (2018)",
-                  "Antwerpen",     2020,    "ELZ Zuid",       1, 27720,    "Antwerpen Kiel (2020)",
-                  "Antwerpen",     2030,    "ELZ Noord",      1,  6821,    "Antwerpen Luchtbal (2030)",
-                  "Antwerpen",     2040,    "ELZ Noord",      1,  9947,    "Antwerpen BEZALI (2040)",
-                  "Antwerpen",     2050,    "ELZ Centrum",    1, 16889,    "Antwerpen Linkerover (2050)",
-                  "Antwerpen",     2060,    "ELZ Centrum",    1, 42953,    "Antwerpen Noord (2060)",
-                  "Deurne",        2100,    "ELZ Oost",       1, 81059,    "Deurne (2100)",
-                  "Ekeren",        2180,    "ELZ Noord",      1, 28370,    "Ekeren (2180)",
-                  "Hoboken",       2660,    "ELZ Zuid",       1, 41093,    "Hoboken (2660)",
-                  "Merksem",       2170,    "ELZ Noord",      1, 44936,    "Merksem (2170)",
-                  "Wilrijk",       2610,    "ELZ Zuid",       1, 41773,    "Wilrijk (2610)",
-                  "Berchem",       2600,    "ELZ Centrum",    1, 43370,    "Berchem (2600)",
-                  "Borgerhout",    2140,    "ELZ Oost",       1, 45906,    "Borgerhout (2140)"
+                  "Antwerpen",     2000,    "ELZ Centrum",    1, 38171,    "Antwerpen Centrum (2000)",
+                  "Antwerpen",     2018,    "ELZ Centrum",    1, 60874,    "Antwerpen Zuid (2018)",
+                  "Antwerpen",     2020,    "ELZ Zuid",       1, 27770,    "Antwerpen Kiel (2020)",
+                  "Antwerpen",     2030,    "ELZ Noord",      1,  6709,    "Antwerpen Luchtbal (2030)",
+                  "Antwerpen",     2040,    "ELZ Noord",      1,  9958,    "Antwerpen BEZALI (2040)",
+                  "Antwerpen",     2050,    "ELZ Centrum",    1, 16900,    "Antwerpen Linkerover (2050)",
+                  "Antwerpen",     2060,    "ELZ Centrum",    1, 42967,    "Antwerpen Noord (2060)",
+                  "Deurne",        2100,    "ELZ Oost",       1, 80932,    "Deurne (2100)",
+                  "Ekeren",        2180,    "ELZ Noord",      1, 28432,    "Ekeren (2180)",
+                  "Hoboken",       2660,    "ELZ Zuid",       1, 41174,    "Hoboken (2660)",
+                  "Merksem",       2170,    "ELZ Noord",      1, 44869,    "Merksem (2170)",
+                  "Wilrijk",       2610,    "ELZ Zuid",       1, 41746,    "Wilrijk (2610)",
+                  "Berchem",       2600,    "ELZ Centrum",    1, 43199,    "Berchem (2600)",
+                  "Borgerhout",    2140,    "ELZ Oost",       1, 45842,    "Borgerhout (2140)"
 
               )
 
